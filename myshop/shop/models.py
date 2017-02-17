36d1ef6a486614f0332abf9ddabbe89dd5bb68e8
Vitalik <si_vitalik@yahoo.com>
2017-02-17 20:02:07 +0200

    Start

---
+++ myshop/shop/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', verbose_name="Категорія")
+    
