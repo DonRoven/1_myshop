81467071d79ad0382de93337389ebe43cea3cda4
Vitalik <si_vitalik@yahoo.com>
2017-02-20 16:10:48 +0200

    base html

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,19 +1,46 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+# Модель категории
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
         ordering = ['name']
-        verbose_name = 'Категорія'
+        verbose_name = 'Категория'
         verbose_name_plural = 'Категорії'
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:ProductListByCategory', args=[self.slug])
 
+
+# Модель продукта
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', verbose_name="Категорія")
-    
+    category = models.ForeignKey(Category, related_name='products', verbose_name="Категория")
+    name = models.CharField(max_length=200, db_index=True, verbose_name="Назва")
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name="Зображення товару")
+    description = models.TextField(blank=True, verbose_name="Опис")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Ціна")
+    stock = models.PositiveIntegerField(verbose_name="На складі")
+    available = models.BooleanField(default=True, verbose_name="Доступий")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        index_together = [
+            ['id', 'slug']
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+    def get_absolute_url(self):
+        return reverse('shop:ProductDetail', args=[self.id, self.slug])
